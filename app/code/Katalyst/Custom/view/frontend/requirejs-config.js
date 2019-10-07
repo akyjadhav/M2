2fa2e7ecc20f85e6efadb4489d35bdade517a977
Akshay Jadhav <ajadhav@katalysttech.com>
2019-10-07 10:26:08 +0530

    Initial commit

---
+++ app/code/Katalyst/Custom/view/frontend/requirejs-config.js
@@ -0,0 +1,11 @@
+var config = {
+config: {
+    mixins: {
+        'Magento_Checkout/js/view/summary/abstract-total': {
+            'Katalyst_Custom/js/view/summary/abstract-total-mixin': true
+        },
+        'Magento_Checkout/js/view/summary/shipping': {
+            'Katalyst_Custom/js/view/summary/shipping-mixin': true
+        },
+    }
+}}; 
---
+++ app/code/Katalyst/Custom/view/frontend/web/js/view/summary/abstract-total-mixin.js
@@ -0,0 +1,18 @@
+define(
+        [
+            'uiComponent',
+            'Magento_Checkout/js/model/step-navigator'
+        ],
+        function (Component, stepNavigator) {
+            "use strict";
+            return function (abstractTotal) {
+                return abstractTotal.extend({
+                    isFullMode: function () {
+                        if (!this.getTotals() || stepNavigator.getActiveItemIndex() === 1) {
+                            return true;
+                        }
+                        return true; //add this line to display forcefully summary in shipping step.
+                    }
+                });
+            }
+        });
